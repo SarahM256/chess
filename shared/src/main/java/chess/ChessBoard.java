ea915595bdf1f5594f72d6e2a8abde07724c9821
SarahM256 <jmeinstein@gmail.com>
2026-01-11 15:29:18 -0700

    ChessBoard tests pass

--- shared/src/main/java/chess/ChessBoard.java
+++ shared/src/main/java/chess/ChessBoard.java
@@ -1,4 +1,7 @@
 package chess;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
 
 /**
  * A chessboard that can hold and rearrange chess pieces.
@@ -7,9 +10,24 @@ package chess;
  * signature of the existing methods.
  */
 public class ChessBoard {
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        ChessBoard that = (ChessBoard) o;
+        return Objects.equals(board, that.board);
+    }
 
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(board);
+    }
+
+    public Map<ChessPosition, ChessPiece> board;
     public ChessBoard() {
-        
+        this.board = new HashMap<>();
+        emptyBoard();
     }
 
     /**
@@ -19,7 +37,7 @@ public class ChessBoard {
      * @param piece    the piece to add
      */
     public void addPiece(ChessPosition position, ChessPiece piece) {
-        throw new RuntimeException("Not implemented");
+        board.put(position, piece);
     }
 
     /**
@@ -30,7 +48,7 @@ public class ChessBoard {
      * position
      */
     public ChessPiece getPiece(ChessPosition position) {
-        throw new RuntimeException("Not implemented");
+        return board.get(position);
     }
 
     /**
@@ -38,6 +56,30 @@ public class ChessBoard {
      * (How the game of chess normally starts)
      */
     public void resetBoard() {
-        throw new RuntimeException("Not implemented");
+        emptyBoard();
+        ChessGame.TeamColor[] colors = {ChessGame.TeamColor.WHITE, ChessGame.TeamColor.BLACK};
+        int[] pieceRows = {1, 8};
+        int[] pawnRows = {2, 7};
+        for(int i = 0; i<=1; i++){
+            board.put(new ChessPosition(pieceRows[i], 1), new ChessPiece(colors[i], ChessPiece.PieceType.ROOK));
+            board.put(new ChessPosition(pieceRows[i], 8), new ChessPiece(colors[i], ChessPiece.PieceType.ROOK));
+            board.put(new ChessPosition(pieceRows[i], 2), new ChessPiece(colors[i], ChessPiece.PieceType.KNIGHT));
+            board.put(new ChessPosition(pieceRows[i], 7), new ChessPiece(colors[i], ChessPiece.PieceType.KNIGHT));
+            board.put(new ChessPosition(pieceRows[i], 3), new ChessPiece(colors[i], ChessPiece.PieceType.BISHOP));
+            board.put(new ChessPosition(pieceRows[i], 6), new ChessPiece(colors[i], ChessPiece.PieceType.BISHOP));
+            board.put(new ChessPosition(pieceRows[i], 4), new ChessPiece(colors[i], ChessPiece.PieceType.QUEEN));
+            board.put(new ChessPosition(pieceRows[i], 5), new ChessPiece(colors[i], ChessPiece.PieceType.KING));
+            for(int j = 1; j<=8; j++){
+                board.put(new ChessPosition(pawnRows[i], j), new ChessPiece(colors[i], ChessPiece.PieceType.PAWN));
+            }
+        }
+    }
+
+    private void emptyBoard(){
+        for(int i = 1; i<=8; i++){
+            for(int j = 1; j<=8; j++){
+                board.put(new ChessPosition(i,j), null);
+            }
+        }
     }
 }
